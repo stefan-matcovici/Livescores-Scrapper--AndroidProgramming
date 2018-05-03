22962945540ea5628354a4c34e1be00511c0826f
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-05-03 10:18:04 +0300

    Changed way of serializing event to work with flask jsonify

--- models/Event.py
+++ models/Event.py
@@ -1,9 +1,10 @@
 import json
 import re
 
+
 class Event:
     def __init__(self, id, homeTeam, awayTeam, homeScore, awayScore, score_link, header):
-        self.id = id[id.index('-')+1:]
+        self.id = id[id.index('-') + 1:]
         self.homeTeam = homeTeam
         self.awayTeam = awayTeam
         self.homeScore = homeScore
@@ -16,3 +17,14 @@ class Event:
 
     def __repr__(self):
         return str(self)
+
+    def __dict__(self):
+        return {
+            "id": self.id,
+            "homeTeam": self.homeTeam,
+            "awayTeam": self.awayTeam,
+            "homeScore": self.homeScore,
+            "awayScore": self.awayScore,
+            "scoreLink": self.scoreLink,
+            "header": self.header.__dict__
+        }
